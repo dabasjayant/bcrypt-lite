2f7e681d0033a3bff2b43cacf4aa768cae9e63c9
Jayant Dabas <jayantd1112@gmail.com>
2024-10-13 19:35:53 -0400

    Update bcrypt-lite.js

--- bcrypt-lite.js
+++ bcrypt-lite.js
@@ -1,43 +1,3 @@
-/*
- Note: This is a modified version of bcrypt.js by Jayant Dabas.
- - Please keep the copyright notice from the original authors of the package.
- - The script now supports 'edge' runtime in sync mode to simplify hash and compare methods.
- - The security of the package remains intact because it follows the original logic.
- Copyright (c) 2024 Jayant Dabas <jayantd1112@gmail.com>
-
-
- Copyright (c) 2012 Nevins Bartolomeo <nevins.bartolomeo@gmail.com>
- Copyright (c) 2012 Shane Girish <shaneGirish@gmail.com>
- Copyright (c) 2014 Daniel Wirtz <dcode@dcode.io>
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
- 1. Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- 3. The name of the author may not be used to endorse or promote products
- derived from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
- * @license bcrypt.js (c) 2013 Daniel Wirtz <dcode@dcode.io>
- * Released under the Apache License, Version 2.0
- * see: https://github.com/dcodeIO/bcrypt.js for details
- */
 (function(global, factory) {
 
     /* AMD */ if (typeof define === 'function' && define["amd"])
@@ -176,12 +136,10 @@
      * @throws {Error} If an argument is illegal
      */
     bcrypt.compareSync = function(s, hash) {
-        console.log(hash.length);
         if (typeof s !== "string" || typeof hash !== "string")
             throw Error("Illegal arguments: "+(typeof s)+', '+(typeof hash));
         if (hash.length !== 60)
             return false;
-        console.log(bcrypt.hashSync(s, hash.substr(0, hash.length-31)), hash);
         return safeStringCompare(bcrypt.hashSync(s, hash.substr(0, hash.length-31)), hash);
     };
 
